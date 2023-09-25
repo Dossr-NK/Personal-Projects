1b7f3d720ca35e7b6e76aa3aee6f1a730b88d5c1
Dossr-NK <nathaniel.kamal13@gmail.com>
2023-09-24 23:06:12 -0700

    Start of Bankg Management System App project.

---
+++ C/Bank Management System App/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/natha/Desktop/Personal-Projects/C/Bank Management System App
+# Build directory: C:/Users/natha/Desktop/Personal-Projects/C/Bank Management System App/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ C/Bank Management System App/src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Hello, from Bank Management System App!\n");
+}
