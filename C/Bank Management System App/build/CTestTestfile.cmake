bc535110fdba5f07fe4816e15de38483337da159
Nathaniel Kamal <110137450+Dossr-NK@users.noreply.github.com>
2023-09-25 00:57:38 -0700

    Delete C/Bank Management System App/build directory

--- C/Bank Management System App/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/natha/Desktop/Personal-Projects/C/Bank Management System App
-# Build directory: C:/Users/natha/Desktop/Personal-Projects/C/Bank Management System App/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
